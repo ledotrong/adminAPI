ff4f4ac9c96a1cfcad71daf8d02f5b72ace53f9d
Trong Ledo <trong.ledo136@gmail.com>
2019-12-03 11:16:37 +0700

    Đăng nhập đăng kí

---
+++ models/validation.js
@@ -0,0 +1,61 @@
+const Joi = require('joi');
+
+const registerValidation = data => {
+  const schema = {
+    name: Joi.string()
+      .required()
+      .min(6),
+    email: Joi.string()
+      .required()
+      .min(6)
+      .email(),
+    password: Joi.string()
+      .required()
+      .min(6),
+    picture: Joi.string(),
+    role: Joi.string().required(),
+    skills: Joi.array().required(),
+    address: Joi.string().required()
+  };
+
+  return Joi.validate(data, schema);
+};
+
+const loginValidation = data => {
+  const schema = {
+    email: Joi.string()
+      .required()
+      .min(6)
+      .email(),
+    password: Joi.string()
+      .required()
+      .min(6)
+  };
+
+  return Joi.validate(data, schema);
+};
+
+const updateValidation = data => {
+  const schema = {
+    name: Joi.string()
+      .required()
+      .min(6),
+    email: Joi.string()
+      .required()
+      .min(6)
+      .email(),
+    password: Joi.string()
+      .required()
+      .min(6),
+    picture: Joi.string(),
+    role: Joi.string().required(),
+    skills: Joi.array().required(),
+    address: Joi.string().required()
+  };
+
+  return Joi.validate(data, schema);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.updateValidation = updateValidation;
