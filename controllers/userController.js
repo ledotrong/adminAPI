b80894097875cfa24a270d6dc200a8a1dae5fbc3
Trong Ledo <trong.ledo136@gmail.com>
2019-12-09 17:17:38 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,6 @@ const User = require('../models/User');
 exports.users = async (req, res) => {
   const userCheck = await User.find({}, (err, users) => {
     console.log(users);
-    res.send('users');
+    res.json(users);
   });
 };
